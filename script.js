f738d65fecab5c08179fb05e6889865e196fa444
Alex <dgirolamo777@gmail.com>
2024-04-29 01:51:37 +0300

    first

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+alert("Hello");
+let answer = prompt("Если вам 18?")
+prompt("Сколько вам лет?");
+console.log(answer);
+
+let num = 9;
+let string ="Hello"; 
+let person = {
+    name: "Alex",
+    age: 42
+}
+
+let arr = [123,'Alexey', 'set'];
+console.log(person.name);
+
